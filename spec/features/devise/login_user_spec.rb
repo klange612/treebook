73b367fbd7e91dbbc7c98c10169b820db37bb06e
Kurt Lange <klange@usinternet.com>
2016-11-11 23:44:04 -0600

    Added rspec/capybara and checks for index, signup and login

---
+++ spec/features/devise/login_user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "user login should" do
+  let!(:user) { User.create(email: "john@a.com", password: "password", password_confirmation: "password")}
+
+  it " login with correct username and password" do
+    visit 'users/sign_in'
+    fill_in('user_email', with: "john@a.com")
+    fill_in('user_password', with: "password")
+    click_button('Log in')
+    expect(page).to have_content("Signed in successfully")
+  end
+
+  it " fail to log in with incorrect username" do
+    visit 'users/sign_in'
+    fill_in('user_email', with: "bob@b.com")
+    fill_in('user_password', with: 'password')
+    click_button('Log in')
+    expect(page).to have_content("Invalid Email or password")
+  end
+
+  it " fail to log in with incorrect password" do
+    visit 'users/sign_in'
+    fill_in('user_email', with: "john@a.com")
+    fill_in('user_password', with: "wrong_pass")
+    click_button('Log in')
+    expect(page).to have_content("Invalid Email or password")
+  end
+
+end
---
+++ spec/features/statuses/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "statuses index page " do
+  let!(:status) { Status.create(content: "My status") }
+
+  it "has title statuses" do
+    set_page
+    within('h1') do
+      expect(page).to have_content("Statuses")
+    end
+  end
+
+  it "has a status of 'My Status'" do
+    set_page
+    expect(page).to have_content("My status")
+  end
+
+  def set_page
+    visit '/statuses'
+  end
+end
